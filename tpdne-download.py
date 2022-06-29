f004c2a15442bdb5a868dae4b3723ec4bbc73084
Simon Oswald <75795978+curosim@users.noreply.github.com>
2022-06-29 03:23:46 +0200

    Update tpdne-download.py

--- tpdne-download.py
+++ tpdne-download.py
@@ -75,7 +75,6 @@ def main():
 	number_of_images = int(input("[*] Enter number of images: "))
 	os.makedirs('./images/', exist_ok=True)
 
-	# Start image download in multiple threads
 	pool = ThreadPool(num_threads)
 	results = pool.map(download_image, range(0, number_of_images))
 	pool.close()
